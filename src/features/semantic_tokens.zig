f5c407b935ee7b4bfb478d69a49b9e88a44b5d61
bob frankel <bob@biosbob.biz>
2024-07-17 14:11:28 -0700

    check to avoid assertion panic

--- src/features/semantic_tokens.zig
+++ src/features/semantic_tokens.zig
@@ -1002,10 +1002,12 @@ fn writeContainerField(builder: *Builder, node: Ast.Node.Index, container_decl: 
     var container_field = tree.fullContainerField(node).?;
 
     const fld_name_tok = container_field.ast.main_token;
-    const fld_name = offsets.identifierTokenToNameSlice(builder.handle.tree, fld_name_tok);
-    if (zigemType(fld_name)) |tok_type| {
-        try writeToken(builder, fld_name_tok, tok_type);
-        return;
+    if (tree.tokens.items(.tag)[fld_name_tok] == .identifier) {
+        const fld_name = offsets.identifierTokenToNameSlice(builder.handle.tree, fld_name_tok);
+        if (zigemType(fld_name)) |tok_type| {
+            try writeToken(builder, fld_name_tok, tok_type);
+            return;
+        }
     }
 
     const field_token_type = fieldTokenType(container_decl, builder.handle, false) orelse .property;
